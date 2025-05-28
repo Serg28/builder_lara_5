d522e1f32a7d8e304f8efee36949d4c84cc59704
serg <privat_tel@mail.ru>
2025-05-28 13:10:24 +0300

    fix: Исправлена фильтрация переводов CMS, добавлены некоторые переводы в шаблон

--- src/resources/views/translation_cms/part/form.blade.php
+++ src/resources/views/translation_cms/part/form.blade.php
@@ -1,5 +1,5 @@
 <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times; </button>
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times;</button>
 
     @if(isset($info->id))
         <h4 class="modal-title" id="modal_form_label">{{__cms("Редактирование")}}</h4>
@@ -8,17 +8,19 @@
     @endif
 </div>
 <div class="modal-body">
-    <form id="form_page" class="smart-form" enctype="multipart/form-data" method="post" novalidate="novalidate" >
-      <fieldset style="padding:0">
+    <form id="form_page" class="smart-form" enctype="multipart/form-data" method="post" novalidate="novalidate">
+        <fieldset style="padding:0">
             <div class="row">
-              <section class="col" style="float: none">
-                <label class="label">Фраза</label>
-                <div style="position: relative;">
-                  <label class="input">
-                  <input type="text" value="{{{ $info->phrase ?? "" }}}" onkeyup="Trans.getTranslate(this.value)" name="phrase"  class="dblclick-edit-input form-control input-sm unselectable">
-                  </label>
-                </div>
-              </section>
+                <section class="col" style="float: none">
+                    <label class="label">{{__cms('Фраза')}}</label>
+                    <div style="position: relative;">
+                        <label class="input">
+                            <input type="text" value="{{{ $info->phrase ?? "" }}}"
+                                   onkeyup="Trans.getTranslate(this.value)" name="phrase"
+                                   class="dblclick-edit-input form-control input-sm unselectable">
+                        </label>
+                    </div>
+                </section>
             </div>
             <div class="row">
 
@@ -41,11 +43,12 @@
                 @endforeach
             </div>
 
-      </fieldset>
+        </fieldset>
         <div class="modal-footer">
-          <i class="fa fa-gear fa-41x fa-spin" style="display: none"></i>
-          <button  type="submit" class="btn btn-success btn-sm"> <span class="glyphicon glyphicon-floppy-disk"></span> {{__cms('Сохранить')}} </button>
-          <button type="button" class="btn btn-default" data-dismiss="modal"> {{__cms('Отмена')}} </button>
+            <i class="fa fa-gear fa-41x fa-spin" style="display: none"></i>
+            <button type="submit" class="btn btn-success btn-sm"><span
+                        class="glyphicon glyphicon-floppy-disk"></span> {{__cms('Сохранить')}} </button>
+            <button type="button" class="btn btn-default" data-dismiss="modal"> {{__cms('Отмена')}} </button>
         </div>
 
         <input type="hidden" name="id" value="{{$info->id ?? "0"}}">
