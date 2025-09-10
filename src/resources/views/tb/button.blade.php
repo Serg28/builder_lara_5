ac5983319b57d006b2e49e66b9636f8cced1746d
serg <privat_tel@mail.ru>
2025-09-10 22:01:56 +0300

    feat(button.blade.php): Add attributes to button element

--- src/resources/views/tb/button.blade.php
+++ src/resources/views/tb/button.blade.php
@@ -3,40 +3,50 @@
         <a onclick="sendAjax('{{$button['link']}}'{{isset($button['massage_start']) ? ", '" . $button['massage_start'] . "'" : ''}})"
            class="btn btn-xs btn-default"
            @if(isset($button['id'])) id="{{ $button['id'] }}" @endif
-        >
-            <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
-            {{__cms($button['caption'])}}
-        </a>
+    @if(!empty($button['attributes']))
+        @foreach($button['attributes'] as $attr => $value)
+            {{ $attr }}="{{ $value }}"
+        @endforeach
+    @endif
+    >
+    <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
+    {{ __cms($button['caption']) }}
+    </a>
 
-        <script>
-            if (typeof sendAjax != 'function') {
-                function sendAjax(link, message_start) {
-					if (message_start != undefined) {
-						TableBuilder.showSuccessNotification(message_start);
-					}
-
-					TableBuilder.showPreloader();
-
-                    $.post(link, {},
-                        function(data){
-                            if (data.status == 'success') {
-                                TableBuilder.showSuccessNotification(data.message);
-                            } else {
-                                TableBuilder.showErrorNotification(data.message);
-                            }
-
-                        }, 'json')
-                        .complete(function () {
-                            TableBuilder.hidePreloader();
-                        });
+    <script>
+        if (typeof sendAjax != 'function') {
+            function sendAjax(link, message_start) {
+                if (message_start !== undefined) {
+                    TableBuilder.showSuccessNotification(message_start);
                 }
-            }
 
-        </script>
+                TableBuilder.showPreloader();
+
+                $.post(link, {},
+                    function (data) {
+                        if (data.status === 'success') {
+                            TableBuilder.showSuccessNotification(data.message);
+                        } else {
+                            TableBuilder.showErrorNotification(data.message);
+                        }
+                    }, 'json')
+                    .always(function () {
+                        TableBuilder.hidePreloader();
+                    });
+            }
+        }
+    </script>
     @else
-    <a href="{{$button['link']}}" class="btn btn-xs btn-default" @if(isset($button['id'])) id="{{ $button['id'] }}" @endif>
-        <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
-        {{__cms($button['caption'])}}
+        <a href="{{ $button['link'] }}" class="btn btn-xs btn-default"
+           @if(isset($button['id'])) id="{{ $button['id'] }}" @endif
+    @if(!empty($button['attributes']))
+        @foreach($button['attributes'] as $attr => $value)
+            {{ $attr }}="{{ $value }}"
+        @endforeach
+    @endif
+    >
+    <i class="fa {{ isset($button['icon']) && $button['icon'] ? "fa-" . $button['icon'] : "" }}"></i>
+    {{ __cms($button['caption']) }}
     </a>
     @endif
 </div>
