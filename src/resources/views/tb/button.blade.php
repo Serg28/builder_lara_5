e1d6fb3fe6c4316cacab5a0c9a67e0aa67e4e8f7
serg <privat_tel@mail.ru>
2025-09-10 22:17:40 +0300

    Update button.blade.php

--- src/resources/views/tb/button.blade.php
+++ src/resources/views/tb/button.blade.php
@@ -1,52 +1,55 @@
-<div class="widget-toolbar" role="menu">
-    @if (isset($button['ajax']))
-        <a onclick="sendAjax('{{$button['link']}}'{{isset($button['massage_start']) ? ", '" . $button['massage_start'] . "'" : ''}})"
-           class="btn btn-xs btn-default"
-           @if(isset($button['id'])) id="{{ $button['id'] }}" @endif
-    @if(!empty($button['attributes']))
-        @foreach($button['attributes'] as $attr => $value)
-            {{ $attr }}="{{ $value }}"
-        @endforeach
-    @endif
-    >
-    <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
-    {{ __cms($button['caption']) }}
-    </a>
+@if($button)
+    <div class="widget-toolbar" role="menu">
+        @if (isset($button['ajax']))
+            <a onclick="sendAjax('{{$button['link']}}'{{isset($button['massage_start']) ? ", '" . $button['massage_start'] . "'" : ''}})"
+               class="btn btn-xs btn-default"
+               @if(isset($button['id'])) id="{{ $button['id'] }}"
+        @endif
+        @if(!empty($button['attributes']))
+            @foreach($button['attributes'] as $attr => $value)
+                {{ $attr }}="{{ $value }}"
+            @endforeach
+        @endif
+        >
+        <i class="fa {{isset($button['icon']) && $button['icon'] ? "fa-".$button['icon'] : ""}}"></i>
+        {{ __cms($button['caption']) }}
+        </a>
 
-    <script>
-        if (typeof sendAjax != 'function') {
-            function sendAjax(link, message_start) {
-                if (message_start !== undefined) {
-                    TableBuilder.showSuccessNotification(message_start);
+        <script>
+            if (typeof sendAjax != 'function') {
+                function sendAjax(link, message_start) {
+                    if (message_start !== undefined) {
+                        TableBuilder.showSuccessNotification(message_start);
+                    }
+
+                    TableBuilder.showPreloader();
+
+                    $.post(link, {},
+                        function (data) {
+                            if (data.status === 'success') {
+                                TableBuilder.showSuccessNotification(data.message);
+                            } else {
+                                TableBuilder.showErrorNotification(data.message);
+                            }
+                        }, 'json')
+                        .always(function () {
+                            TableBuilder.hidePreloader();
+                        });
                 }
-
-                TableBuilder.showPreloader();
-
-                $.post(link, {},
-                    function (data) {
-                        if (data.status === 'success') {
-                            TableBuilder.showSuccessNotification(data.message);
-                        } else {
-                            TableBuilder.showErrorNotification(data.message);
-                        }
-                    }, 'json')
-                    .always(function () {
-                        TableBuilder.hidePreloader();
-                    });
             }
-        }
-    </script>
-    @else
-        <a href="{{ $button['link'] }}" class="btn btn-xs btn-default"
-           @if(isset($button['id'])) id="{{ $button['id'] }}" @endif
-    @if(!empty($button['attributes']))
-        @foreach($button['attributes'] as $attr => $value)
-            {{ $attr }}="{{ $value }}"
-        @endforeach
-    @endif
-    >
-    <i class="fa {{ isset($button['icon']) && $button['icon'] ? "fa-" . $button['icon'] : "" }}"></i>
-    {{ __cms($button['caption']) }}
-    </a>
-    @endif
-</div>
+        </script>
+        @else
+            <a href="{{ $button['link'] }}" class="btn btn-xs btn-default"
+               @if(isset($button['id'])) id="{{ $button['id'] }}" @endif
+        @if(!empty($button['attributes']))
+            @foreach($button['attributes'] as $attr => $value)
+                {{ $attr }}="{{ $value }}"
+            @endforeach
+        @endif
+        >
+        <i class="fa {{ isset($button['icon']) && $button['icon'] ? "fa-" . $button['icon'] : "" }}"></i>
+        {{ __cms($button['caption']) }}
+        </a>
+        @endif
+    </div>
+@endif
