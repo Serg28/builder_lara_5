72bdd32376743e2fea7dee3ea7d3b9498fa65520
Sergey <privat_tel@mail.ru>
2025-09-11 17:35:07 +0300

    Update helpers.php

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -42,7 +42,15 @@ if (! function_exists('languagesOfSite')) {
         }
     }
 }
+if (! function_exists('adminLang')) {
+    function adminLang() : string
+    {
+        $lang = Cookie::get('lang_admin') ?: config('builder.translations.cms.language_default');
 
+        // совместимость со старым кодом
+        return $lang === 'uk' ? 'ua' : $lang;
+    }
+}
 /*
 if (! function_exists('setting')) {
 
