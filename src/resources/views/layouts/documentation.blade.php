1bbe02db1ff772debf30556b3ffe7bcd7121c889
serg <privat_tel@mail.ru>
2025-09-18 12:56:58 +0300

    fix(documentation): css path

--- src/resources/views/layouts/documentation.blade.php
+++ src/resources/views/layouts/documentation.blade.php
@@ -7,8 +7,8 @@
     <meta name="description" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" type="text/css" href="/packages/vis/builder/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="/packages/vis/builder/css/documentation-page.min.css">
+    <link rel="stylesheet" type="text/css" href="/packages/linecore/builder/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/packages/linecore/builder/css/documentation-page.min.css">
     <meta http-equiv="Content-Security-Policy"
           content="default-src 'self' 'unsafe-inline' data: https:; img-src 'self' data: https: *; style-src 'self' 'unsafe-inline' https:; script-src 'self' 'unsafe-inline' https:">
 
