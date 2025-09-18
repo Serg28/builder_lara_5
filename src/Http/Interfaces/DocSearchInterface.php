f7feeb482aeef8468f1a1c0b43089e319e4fd3ff
serg <privat_tel@mail.ru>
2025-09-18 11:02:42 +0300

    add: documentation editor&&page

---
+++ src/Http/Interfaces/DocSearchInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Vis\Builder\Interfaces;
+
+interface DocSearchInterface
+{
+    /**
+     * Поиск текста в файлах
+     *
+     * @param string $query     Что ищем
+     * @param string $path      Каталог для обхода
+     * @param string $extension Расширение файлов (например, 'html')
+     * @return array            Массив результатов [['file' => ..., 'matches' => [...]], ...]
+     */
+    public function search(string $query, string $path, string $extension = 'html'): array;
+
+    /**
+     * Создание сниппета (короткого отрывка) из текста с подсвечиванием поискового запроса
+     * 
+     * @param string $text
+     * @param string $q
+     * @param int $len
+     * @return string
+     */
+    public function snippet(string $text, string $q, int $len = 150): string;
+}
