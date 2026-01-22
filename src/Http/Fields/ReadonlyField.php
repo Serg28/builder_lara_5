bdd781cc05a2493712a895b276cf704b67c046f3
serg <privat_tel@mail.ru>
2026-01-22 12:25:07 +0200

    upd: readonly field

--- src/Http/Fields/ReadonlyField.php
+++ src/Http/Fields/ReadonlyField.php
@@ -2,7 +2,40 @@
 
 namespace Vis\Builder\Fields;
 
+use Closure;
+
+/*
+ * Приклад з форматуванням виводу:
+ 
+ReadonlyField::make('Наявність на складах', 'availability_info')
+    ->onlyForm()
+    ->handleUsing(function ($value) {
+        if(!empty($value->availability_info)) {
+                return collect(json_decode($value->availability_info, true))
+                    ->map(fn($v) => $v['title'].': <strong>'.$v['quantity'].'</strong>')
+                    ->implode(', ');
+        }
+        return __cms('Немає інформації');
+    }),
+*/
+
 class ReadonlyField extends Field
 {
+    protected ?Closure $callback = null;
+
+    public function handleUsing(Closure $callback)
+    {
+        $this->callback = $callback;
+        return $this;
+    }
+
+    public function setValue($value)
+    {
+        if (is_callable($this->callback)) {
+            $this->value = call_user_func($this->callback, $value);
+        } else {
+            parent::setValue($value);
+        }
+    }
 
 }
