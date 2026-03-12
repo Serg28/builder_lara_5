84bca183f732a5a51be296f006b0b5191ad5c4f1
Sergey <privat_tel@mail.ru>
2026-03-12 17:23:00 +0200

    Fix namespace for Artisan calls in ResetCacheController

--- src/Http/ControllersNew/ResetCacheController.php
+++ src/Http/ControllersNew/ResetCacheController.php
@@ -6,11 +6,11 @@ class ResetCacheController
 {
     public function index() {
         opcache_reset();
-        Artisan::call('cache:clear');
-        Artisan::call('config:clear');
-        Artisan::call('route:clear');
-        Artisan::call('view:clear');
-        Artisan::call('cache:clear_rebuild');
+        \Artisan::call('cache:clear');
+        \Artisan::call('config:clear');
+        \Artisan::call('route:clear');
+        \Artisan::call('view:clear');
+        \Artisan::call('cache:clear_rebuild');
         return __cms('Кеш успішно очищено');
     }
 }
