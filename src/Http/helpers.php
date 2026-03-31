84ca4bd27ab646a2d3c0f4148886d4cd15a1759f
Sergey <privat_tel@mail.ru>
2026-03-31 14:27:13 +0300

    Add default language fallback to 'uk' in helpers.php

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -174,7 +174,7 @@ if (! function_exists('__cms')) {
             // $thisLang = Cookie::get('lang_admin', config('builder.translations.cms.language_default'));
             // $thisLang = adminLang(false);
             // $thisLang = config('builder.translations.cms.language_default', 'uk');
-            $thisLang = Cookie::get('lang_admin') ?: config('builder.translations.cms.language_default');
+            $thisLang = Cookie::get('lang_admin') ?: config('builder.translations.cms.language_default', 'uk');
 
             $arrayTranslate = TranslationsPhrasesCms::fillCacheTrans();
 
