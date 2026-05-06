4f14fad0f62d2908e970a4d695ce0376f7dc0003
Sergey <privat_tel@mail.ru>
2026-05-06 17:28:16 +0300

    Refactor setting function with static caching
    Refactor the 'setting' function to use static caching for improved performance and error handling.

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -52,15 +52,6 @@ if (! function_exists('adminLang')) {
 }
 /*
 if (! function_exists('setting')) {
-
-    function setting(string $slug)
-    {
-        return Cache::tags('settings')->rememberForever($slug . App::getLocale(), function() use ($slug) {
-            return (new Settings())->model()->getValue($slug);
-        });
-    }
-}*/
-if (! function_exists('setting')) {
     function setting(string $slug)
     {
         try {
@@ -75,6 +66,32 @@ if (! function_exists('setting')) {
             // Перехватываем любые другие ошибки (например, связанные с кешем)
             return null;
         }
+    }
+}
+*/
+if (! function_exists('setting')) {
+    function setting(string $slug)
+    {
+        static $loadedConfigs = [];
+
+        $key = 'setting_'.$slug . App::getLocale();
+
+        if (array_key_exists($key, $loadedConfigs)) {
+            $value = $loadedConfigs[$key];
+        } else {
+            try {
+                $value = Cache::tags(['settings'])->remember($key, 3600, function() use ($slug) {
+                    return (new Settings())->model()->getValue($slug);
+                });
+                $loadedConfigs[$key] = $value;
+            } catch (QueryException $e) {
+                return null;
+            } catch (Exception $e) {
+                return null;
+            }
+        }
+
+        return $value;
     }
 }
 
