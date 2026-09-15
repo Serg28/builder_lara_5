5c6a8ad9848ecf59204c99686a6d80786a3e0e4e
Sergey <privat_tel@mail.ru>
2026-09-15 10:37:29 +0300

    Optimize translation caching and HTML fixing

--- src/Http/Traits/TranslateTrait.php
+++ src/Http/Traits/TranslateTrait.php
@@ -6,37 +6,45 @@ use Illuminate\Support\Facades\App;
 
 trait TranslateTrait
 {
+    protected $translatedCache = [];
+
     public function t($ident)
     {
-        $this->$ident = preg_replace("/[\r\n]+/", "\\r\\n", (string)$this->$ident);
-        $this->$ident = str_replace("\t", '\t', (string)$this->$ident);
-
-        $fieldArray = json_decode($this->$ident);
         $lang = App::getLocale();
+        $key = "{$ident}_{$lang}";
 
-        return $fieldArray->$lang ?? '';
+        return $this->translatedCache[$key] ??= $this->translateField($ident, $lang);
+    }
+
+    protected function translateField(string $ident, string $lang): string
+    {
+        $value = $this->getAttributes()[$ident] ?? $this->{$ident} ?? null;
+        $value = preg_replace("/[\r\n]+/", "\\r\\n", (string) $value);
+        $value = str_replace("\t", '\t', $value);
+
+        return json_decode($value)->{$lang} ?? '';
     }
 
     public function t_htmlfix($ident)
     {
         $content = nl2br($this->t($ident));
-    
+
         $content = preg_replace_callback('/<table[^>]*>.*?<\/table>/s', function($match) {
             return preg_replace('/<br\s*\/?>/i', '', $match[0]);
         }, $content);
-    
+
         $content = str_replace(["\r\n", "\n\r", "\\r\\n", "\\n\\r", "\r", "\n"], '', $content);
         $content = str_replace(['</tr><br /><tr>', '</tr><br><tr>', '</tr><br> <tr>', '</tr><br/> <tr>'], '</tr><tr>', $content);
         $content = str_replace(['</li><br /><li>', '</li><br><li>'], '</li><li>', $content);
-    
+
         if ($content) {
             $encoding = mb_detect_encoding($content);
             $doc = new \DOMDocument('', $encoding);
-    
+
             @$doc->loadHTML('<html><head>'
                 . '<meta http-equiv="content-type" content="text/html; charset='
                 . $encoding . '"></head><body>' . trim($content) . '</body></html>');
-    
+
             $nodes = $doc->getElementsByTagName('body')->item(0)->childNodes;
             $html = '';
             $len = $nodes->length;
@@ -45,7 +53,7 @@ trait TranslateTrait
             }
             return $html;
         }
-    
+
         return $content;
     }
 }
